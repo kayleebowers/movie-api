8b57e8e0a3677a77c4a60d3bf6105234f6426e6f
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-06-13 20:39:05 +0000

    style: format server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const http = require('http'),
-    url = require('url'),
-    fs = require('fs');
+const http = require("http"),
+  url = require("url"),
+  fs = require("fs");
 
 //create server with http module
 
@@ -8,37 +8,40 @@ http.createServer((request, response) => {
     //parse url
     let address = request.url;
     parsedUrl = new URL(address, true);
-    filePath = '';
+    filePath = "";
 
     //add requests to log
-    fs.appendFile('log.txt', `URL: ${address} 
-        Timestamp: ${new Date}
+    fs.appendFile(
+      "log.txt",
+      `URL: ${address} 
+        Timestamp: ${new Date()}
         
-        `, 
-        (err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('Added to log');
-            }
-        });
+        `,
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Added to log");
+        }
+      }
+    );
 
     //check for filePath and read file
-    if (parsedUrl.pathname.includes('documentation')) {
-        filePath = (__dirname + './documentation.html');
+    if (parsedUrl.pathname.includes("documentation")) {
+      filePath = __dirname + "./documentation.html";
     } else {
-        filePath = 'index.html';
+      filePath = "index.html";
     }
 
     fs.readFile(filePath, (err, data) => {
-        if (err) {
-            throw err;
-        }
-        response.writeHead(200, {'Content-Type': 'text/plain'});
-        response.write(data);
-        response.end('Hello Node!\n');
+      if (err) {
+        throw err;
+      }
+      response.writeHead(200, { "Content-Type": "text/plain" });
+      response.write(data);
+      response.end("Hello Node!\n");
     });
+  })
+  .listen(8080);
 
-}).listen(8080);
-
-console.log('My first Node test server is running on Port 8080.');
+console.log("My first Node test server is running on Port 8080.");
