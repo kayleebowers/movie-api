79f3e4b2dd21209d07d882dbf7e4aa38b60ae52e
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-06-15 15:33:20 +0000

    style: format index.js

--- index.js
+++ index.js
@@ -1,52 +1,53 @@
-const express = require('express');
-    morgan = require('morgan');
-   
+const express = require("express");
+morgan = require("morgan");
+
 const app = express();
 
 let topMovies = [
-    {
-        title: 'The Shawshank Redemption',
-        director: 'Frank Darabont'
-    }, 
-    {
-        title: 'The Godfather',
-        director: 'Francis Ford Coppola'
-    }, 
-    {
-        title: 'The Dark Knight',
-        director: 'Christopher Nolan'
-    }, 
-    {
-        title: '12 Angry Men',
-        director: 'Sidney Lumet'
-    }, 
-    {
-        title: 'Schindler\'s List',
-        director: 'Steven Spielberg'
-    }, 
-    {
-        title: 'The Lord of the Rings: The Return of the King',
-        director: 'Peter Jackson'
-    }, 
-    {
-        title: 'Pulp Fiction',
-        director: 'Quentin Tarantino'
-    }, 
-    {
-        title: 'The Good, the Bad and the Ugly',
-        director: 'Sergio Leone'
-    }, 
-    {
-        title: 'Spider-Man: Across the Spider-Verse',
-        director: ['Joaquim Dos Santos', 'Kemp Powers', 'Justin K. Thompson']
-    }, 
-    {
-        title: 'Forrest Gump',
-        director: 'Robert Zemeckis'
-    }
+  {
+    title: "The Shawshank Redemption",
+    director: "Frank Darabont",
+  },
+  {
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+  },
+  {
+    title: "The Dark Knight",
+    director: "Christopher Nolan",
+  },
+  {
+    title: "12 Angry Men",
+    director: "Sidney Lumet",
+  },
+  {
+    title: "Schindler's List",
+    director: "Steven Spielberg",
+  },
+  {
+    title: "The Lord of the Rings: The Return of the King",
+    director: "Peter Jackson",
+  },
+  {
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+  },
+  {
+    title: "The Good, the Bad and the Ugly",
+    director: "Sergio Leone",
+  },
+  {
+    title: "Spider-Man: Across the Spider-Verse",
+    director: ["Joaquim Dos Santos", "Kemp Powers", "Justin K. Thompson"],
+  },
+  {
+    title: "Forrest Gump",
+    director: "Robert Zemeckis",
+  },
 ];
 
-//
-app.get('/movies', (req, res) => {
-    res.json(topMovies);
-})
+// get list of top movies from /movies endpoint
+app.get("/movies", (req, res) => {
+  res.json(topMovies);
+});
+
