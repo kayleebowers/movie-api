401bb54d4485edf4d5e7f7d47e179d094fc05eaa
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-06-26 22:27:17 +0000

    feat: add movie model to models.js

---
+++ models.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+let movieSchema = mongoose.Schema({
+    Title: { type: String, required: true },
+    Description: { type: String, required: true },
+    Genre: {
+        Name: String,
+        Description: String
+    },
+    Director: {
+        Name: String,
+        Bio: String
+    },
+    Actors: [String],
+    ImagePath: String,
+    Featured: Boolean
+});
+
