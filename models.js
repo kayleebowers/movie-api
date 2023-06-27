60940958d7ff6c4bd2e55aba307a348510cca0a9
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-06-27 01:29:38 +0000

    feat: add schema model for user and movie

--- models.js
+++ models.js
@@ -16,3 +16,16 @@ let movieSchema = mongoose.Schema({
     Featured: Boolean
 });
 
+let userSchema = mongoose.Schema({
+    Username: {type: String, required: true},
+    Password: {type: String, required: true},
+    Email: {type: String, required: true},
+    Birthday: Date,
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+});
+
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
