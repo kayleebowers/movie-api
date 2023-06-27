69be44073be0b0c2eced56517dd5148b7247b17c
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-06-27 19:04:54 +0000

    feat: add user with mongoose model call

--- index.js
+++ index.js
@@ -57,16 +57,39 @@ app.get("/directors/:name", (req, res) => {
 })
 
 // Allow new users to register; —POST /register 
+// Expected body format: 
+// {
+//   ID: Integer,
+//   Username: String,
+//   Password: String,
+//   Email: String,
+//   Birthday: Date
+// }
 app.post("/register", (req, res) => {
-  let newUser = req.body;
-
-  if (!newUser.name) {
-    res.status(400).send('No name found');
-  } else {
-    users.push(newUser);
-    res.status(200).send('You are registered')
-  }
-})
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + " already exists");
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+        .then((user) => { res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send("Error: " + error)
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("Error: " + error)
+    });
+});
 
 // Allow users to update their user info (username); —PUT /register/:name
 app.put("/register/:name/:id", (req, res) => {
