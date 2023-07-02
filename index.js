91f354927d1298238580fab88b27f3caf59f1ebd
kayleebowers <114454094+kayleebowers@users.noreply.github.com>
2023-07-02 00:56:54 +0000

    fix: update port for heroku

--- index.js
+++ index.js
@@ -292,6 +292,7 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something went wrong");
 });
 
-app.listen(8080, () => {
-  console.log("App is listening on port 8080");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
